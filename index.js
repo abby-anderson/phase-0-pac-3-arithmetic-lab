c782ad5b9f324cbe88b57c4dbea95428f9676d63
Abby Anderson <abby.e.anderson@gmail.com>
2021-08-16 10:41:11 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function increment(n) {
+    return n += 1;
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(string) {
+    return parseInt(string,10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
